baf1279710bc03917c57b142be068416cd805785
Derek <chibie020@gmail.com>
2023-05-13 12:57:08 -0700

    initial commit

---
+++ src/test/java/com/example/springrailwaytest/SpringrailwaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrailwaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrailwaytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
