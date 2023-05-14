5e2c815b48a2f59fc3fc8ab1e72a1dcbe4e822b0
Derek <chibie020@gmail.com>
2023-05-13 19:13:51 -0700

    third commit

--- src/main/java/com/example/springrailwaytest/controller/HomeController.java
+++ src/main/java/com/example/springrailwaytest/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"/", "/home", "/status"})
     public String getStatus() {
-        return "Application is up and running";
+        return "Application is up and running ";
     }
 }
